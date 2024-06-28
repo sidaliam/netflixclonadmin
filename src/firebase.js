568e57608192496a0e00af8974b669853e892aea
sidaliam <sidaliamo18@gmail.com>
2024-06-28 14:59:26 +0100

    changement key firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,16 @@
 import firebase from "firebase";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDScrrNfuRpcDNWewAvYBg8s9X9qB6TyP4",
-  authDomain: "clone-netflix-886c3.firebaseapp.com",
-  projectId: "clone-netflix-886c3",
-  storageBucket: "clone-netflix-886c3.appspot.com",
-  messagingSenderId: "441945427794",
-  appId: "1:441945427794:web:8a7eaf2486d7c5201b748b",
-  measurementId: "G-Y3SSGP7QV2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
